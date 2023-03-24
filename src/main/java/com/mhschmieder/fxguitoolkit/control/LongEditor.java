d4072d91261670d9ac7513f3d5ee1adeab3bf9f8
Mark Schmieder <markhschmieder@gmail.com>
2023-03-23 22:55:26 -0400

    Added custom controls for long values
    An oversight earlier, as the controls for ints don't cover longs. Didn't do one for spinners though, as JavaFX Core doesn't have proper support for that unless we either use the Double based facilities and cast to Long or home-grow equivalents to the Integer based facilities used for the IntegerSpinnerStringConverter.

--- src/main/java/com/mhschmieder/fxguitoolkit/control/LongEditor.java
+++ src/main/java/com/mhschmieder/fxguitoolkit/control/LongEditor.java
@@ -37,55 +37,56 @@ import org.apache.commons.math3.util.FastMath;
 import com.mhschmieder.commonstoolkit.util.ClientProperties;
 
 import javafx.application.Platform;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.scene.input.KeyCode;
 
 /**
- * This class formalizes aspects of text editing that are specific to integers.
+ * This class formalizes aspects of text editing that are specific to longs.
  */
-public class IntegerEditor extends NumberEditor {
+public class LongEditor extends NumberEditor {
+
 
     // Cache the minimum allowed data value (negative).
-    protected int                 _minimumValue;
+    protected long                _minimumValue;
 
     // Cache the maximum allowed data value (positive).
-    protected int                 _maximumValue;
+    protected long                _maximumValue;
 
     // Cache the default data value.
-    protected int                 _defaultValue;
+    protected long                _defaultValue;
 
     // The amount to increment or decrement by, using the arrow keys.
-    protected int                 _valueIncrement;
+    protected long                _valueIncrement;
 
     // Cache the raw numeric representation of the data value.
     // NOTE: This field has to follow JavaFX Property Beans conventions.
-    private final IntegerProperty value;
+    private final LongProperty value;
 
-    public IntegerEditor( final ClientProperties clientProperties,
-                          final String initialText,
-                          final String tooltipText,
-                          final boolean applyToolkitCss ) {
-        this( clientProperties, initialText, tooltipText, applyToolkitCss, -Integer.MAX_VALUE, Integer.MAX_VALUE );
+    public LongEditor( final ClientProperties clientProperties,
+                       final String initialText,
+                       final String tooltipText,
+                       final boolean applyToolkitCss ) {
+        this( clientProperties, initialText, tooltipText, applyToolkitCss, -Long.MAX_VALUE, Long.MAX_VALUE );
     }
 
-    public IntegerEditor( final ClientProperties clientProperties,
-                          final String initialText,
-                          final String tooltipText,
-                          final boolean applyToolkitCss,
-                          final int minimumValue,
-                          final int maximumValue ) {
-        this( clientProperties, initialText, tooltipText, applyToolkitCss, minimumValue, maximumValue, 0, 0 );
+    public LongEditor( final ClientProperties clientProperties,
+                       final String initialText,
+                       final String tooltipText,
+                       final boolean applyToolkitCss,
+                       final long minimumValue,
+                       final long maximumValue ) {
+        this( clientProperties, initialText, tooltipText, applyToolkitCss, minimumValue, maximumValue, 0L, 0L );
     }
 
-    public IntegerEditor( final ClientProperties clientProperties,
-                          final String initialText,
-                          final String tooltipText,
-                          final boolean applyToolkitCss,
-                          final int minimumValue,
-                          final int maximumValue,
-                          final int defaultValue,
-                          final int valueIncrement ) {
+    public LongEditor( final ClientProperties clientProperties,
+                       final String initialText,
+                       final String tooltipText,
+                       final boolean applyToolkitCss,
+                       final long minimumValue,
+                       final long maximumValue,
+                       final long defaultValue,
+                       final long valueIncrement ) {
         // Always call the superclass constructor first!
         super( clientProperties, initialText, tooltipText, applyToolkitCss );
 
@@ -96,9 +97,9 @@ public class IntegerEditor extends NumberEditor {
 
         _valueIncrement = valueIncrement;
 
-        value = new SimpleIntegerProperty( _defaultValue );
+        value = new SimpleLongProperty( _defaultValue );
 
-        _reset = () -> setText( Integer.toString( _defaultValue ) );
+        _reset = () -> setText( Long.toString( _defaultValue ) );
 
         try {
             initEditor();
@@ -119,7 +120,7 @@ public class IntegerEditor extends NumberEditor {
             else {
                 // If limits were established, enforce them. Always check
                 // though, to avoid overflow and underflow.
-                final int clampedValue = getClampedValue( newValue.intValue() );
+                final long clampedValue = getClampedValue( newValue.intValue() );
 
                 // Format the number to match how we display committed values.
                 updateText( clampedValue );
@@ -153,14 +154,14 @@ public class IntegerEditor extends NumberEditor {
                 break;
             case UP:
                 // Increment the current value by the set amount.
-                if ( _valueIncrement != 0 ) {
+                if ( _valueIncrement != 0L ) {
                     setValue( getValue() + _valueIncrement );
                 }
 
                 break;
             case DOWN:
                 // Decrement the current value by the set amount.
-                if ( _valueIncrement != 0 ) {
+                if ( _valueIncrement != 0L ) {
                     setValue( getValue() - _valueIncrement );
                 }
 
@@ -175,16 +176,16 @@ public class IntegerEditor extends NumberEditor {
     @Override
     public String getAllowedCharacters() {
         // Restrict keyboard input to numerals, sign, and delimiters.
-        final String allowedCharacters = ( _minimumValue < 0 )
-            ? ( _maximumValue > 0 ) ? "[0-9.,+-]" : "[0-9.,-]"
-            : ( _maximumValue > 0 ) ? "[0-9.,+]" : "[0-9.,]";
+        final String allowedCharacters = ( _minimumValue < 0L )
+            ? ( _maximumValue > 0L ) ? "[0-9.,+-]" : "[0-9.,-]"
+            : ( _maximumValue > 0L ) ? "[0-9.,+]" : "[0-9.,]";
         return allowedCharacters;
     }
 
     @Override
     public final String getDecoratedText() {
         // Get the most recently committed value.
-        final int savedValue = getValue();
+        final long savedValue = getValue();
 
         // Show the number with units to indicate we committed edits.
         final String formattedText = toString( savedValue );
@@ -197,7 +198,7 @@ public class IntegerEditor extends NumberEditor {
 
     // NOTE: This is an opportunity to pre-parse the typed text before
     // converting to a number, such as when we disallow positive numbers (e.g.).
-    public String getDecoratedText( final int savedValue, final String savedText ) {
+    public String getDecoratedText( final long savedValue, final String savedText ) {
         final String decoratedText = savedText;
 
         return decoratedText;
@@ -206,13 +207,13 @@ public class IntegerEditor extends NumberEditor {
     @Override
     public final void updateText() {
         // Get the most recently committed value.
-        final int savedValue = getValue();
+        final long savedValue = getValue();
 
         // Update the displayed text to match the cached value.
         updateText( savedValue );
     }
 
-    public final void updateText( final int savedValue ) {
+    public final void updateText( final long savedValue ) {
         // Show the number with units to indicate we committed edits.
         final String formattedValue = toString( savedValue );
 
@@ -224,83 +225,83 @@ public class IntegerEditor extends NumberEditor {
     public final void clampValue() {
         // Get the clamped, edited value, stripped of decorations and
         // formatting.
-        final int clampedValue = getClampedValue();
+        final long clampedValue = getClampedValue();
 
         // Update the cached property from the clamped, edited value.
         setValue( clampedValue );
     }
 
-    public final int getClampedValue() {
+    public final long getClampedValue() {
         // The fromString method performs input validation.
         final String undecoratedText = getUndecoratedText();
-        final int clampedValue = fromString( undecoratedText );
+        final long clampedValue = fromString( undecoratedText );
 
         return clampedValue;
     }
 
-    public int getClampedValue( final int unclampedValue ) {
-        final int clampedValue =
+    public long getClampedValue( final long unclampedValue ) {
+        final long clampedValue =
                 FastMath.min( FastMath.max( unclampedValue, _minimumValue ), _maximumValue );
         return clampedValue;
     }
 
-    public final int getMinimumValue() {
+    public final long getMinimumValue() {
         return _minimumValue;
     }
 
-    public final void setMinimumValue( final int minimumValue ) {
+    public final void setMinimumValue( final long minimumValue ) {
         _minimumValue = minimumValue;
     }
 
-    public final int getMaximumValue() {
+    public final long getMaximumValue() {
         return _maximumValue;
     }
 
-    public final void setMaximumValue( final int maximumValue ) {
+    public final void setMaximumValue( final long maximumValue ) {
         _maximumValue = maximumValue;
     }
 
-    public final void setValueIncrement( final int pValueIncrement ) {
+    public final void setValueIncrement( final long pValueIncrement ) {
         _valueIncrement = pValueIncrement;
     }
 
-    public final int getValue() {
+    public final long getValue() {
         return value.get();
     }
 
-    public final void setValue( final int pValue ) {
+    public final void setValue( final long pValue ) {
         value.set( pValue );
     }
 
-    public final IntegerProperty valueProperty() {
+    public final LongProperty valueProperty() {
         return value;
     }
 
     /**
-     * Converts the specified {@link String} into its integer value.
+     * Converts the specified {@link String} into its long value.
      * <p>
      * A {@code null}, empty, or otherwise invalid argument returns zero and
      * also executes the editor reset callback, if any.
      *
      * @param stringValue
      *            The {@link String} to convert
-     * @return The integer value of {@code stringValue}
+     * @return The long value of {@code stringValue}
      * @see #setReset
      */
-    public int fromString( final String stringValue ) {
+    public long fromString( final String stringValue ) {
         // Return with current value vs. penalizing user for internal errors.
-        final int currentValue = getValue();
+        final long currentValue = getValue();
         if ( ( stringValue == null ) || stringValue.trim().isEmpty() ) {
             return currentValue;
         }
 
         // If the user typed a formatted number with units, parse it exactly;
         // otherwise strip the units and try to directly convert the string to
-        // an integer.
-        int intValue = currentValue;
+        // a long.
+        long longValue = currentValue;
         try {
             final Number numericValue = _numberParse.parse( stringValue );
-            intValue = numericValue.intValue();
+            longValue = numericValue.intValue();
         }
         catch ( final ParseException pe ) {
             final int measurementUnitIndex = stringValue.indexOf( _measurementUnitString );
@@ -308,7 +309,7 @@ public class IntegerEditor extends NumberEditor {
                 final String numericString = ( measurementUnitIndex < 0 )
                     ? stringValue
                     : stringValue.substring( 0, measurementUnitIndex + 1 );
-                intValue = Integer.parseInt( numericString );
+                longValue = Long.parseLong( numericString );
             }
             catch ( IndexOutOfBoundsException | NumberFormatException | NullPointerException e ) {
                 if ( _reset != null ) {
@@ -320,27 +321,27 @@ public class IntegerEditor extends NumberEditor {
         // If limits were established, enforce them by range-checking and
         // restricting the parsed or defaulted value. Always check though, to
         // avoid overflow and underflow conditions.
-        final int clampedValue = getClampedValue( intValue );
+        final long clampedValue = getClampedValue( longValue );
 
         return clampedValue;
     }
 
     /**
-     * Converts the specified integer into its {@link String} form.
+     * Converts the specified long into its {@link String} form.
      * <p>
      * A {@code null} argument is converted into the default value.
      *
-     * @param intValue
-     *            The integer to convert
-     * @return The {@link String} form of {@code intValue}
+     * @param longValue
+     *            The long to convert
+     * @return The {@link String} form of {@code longValue}
      */
-    public final String toString( final int intValue ) {
+    public final String toString( final long longValue ) {
         // Do a simple string conversion to a number, in case we get arithmetic
         // exceptions using the number formatter.
-        String stringValue = Integer.toString( intValue );
+        String stringValue = Long.toString( longValue );
 
         try {
-            stringValue = _numberFormat.format( intValue );
+            stringValue = _numberFormat.format( longValue );
         }
         catch ( final ArithmeticException ae ) {
             ae.printStackTrace();
--- src/main/java/com/mhschmieder/fxguitoolkit/control/LongSelector.java
+++ src/main/java/com/mhschmieder/fxguitoolkit/control/LongSelector.java
@@ -40,20 +40,16 @@ import javafx.scene.input.KeyEvent;
 
 /**
  * This class formalizes aspects of list selection that are specific to
- * integer value sets.
- * <p>
- * TODO: Make a long version, but there isn't library support in a few
- *  critical places in terms of using this implementation as a template.
- *  Do we need to use Double factory methods and classes and cast to Long?
+ * long value sets.
  */
-public class IntegerSelector extends NumberSelector {
+public class LongSelector extends NumberSelector {
 
-    public IntegerSelector( final ClientProperties clientProperties,
-                            final boolean useLocale,
-                            final String tooltipText,
-                            final boolean applyToolkitCss,
-                            final boolean editable,
-                            final boolean searchable ) {
+    public LongSelector( final ClientProperties clientProperties,
+                         final boolean useLocale,
+                         final String tooltipText,
+                         final boolean applyToolkitCss,
+                         final boolean editable,
+                         final boolean searchable ) {
         // Always call the superclass constructor first!
         super( clientProperties,
                0,
@@ -67,15 +63,15 @@ public class IntegerSelector extends NumberSelector {
                searchable );
     }
 
-    public IntegerSelector( final ClientProperties clientProperties,
-                            final boolean useLocale,
-                            final String tooltipText,
-                            final boolean applyToolkitCss,
-                            final boolean editable,
-                            final boolean searchable,
-                            final int minimumValue,
-                            final int maximumValue,
-                            final int increment ) {
+    public LongSelector( final ClientProperties clientProperties,
+                         final boolean useLocale,
+                         final String tooltipText,
+                         final boolean applyToolkitCss,
+                         final boolean editable,
+                         final boolean searchable,
+                         final long minimumValue,
+                         final long maximumValue,
+                         final long increment ) {
         this( clientProperties, 
               useLocale, 
               tooltipText, 
@@ -92,18 +88,20 @@ public class IntegerSelector extends NumberSelector {
     }
 
     @SuppressWarnings("nls")
-    private final void initComboBox( final int minimumValue,
-                                     final int maximumValue,
-                                     final int increment ) {
+    private final void initComboBox( final long minimumValue,
+                                     final long maximumValue,
+                                     final long increment ) {
         // Put together the monotonically increasing list of choices.
+        // NOTE: Static arrays can't be allocated with sizes that fit in long
+        //  vs. int, so we use int here, but we should switch to a collection.
         final int numberOfChoices = ( int ) FastMath
                 .floor( ( ( maximumValue - minimumValue ) + 1 ) / increment );
-        final String[] integerValues = new String[ numberOfChoices ];
+        final String[] longValues = new String[ numberOfChoices ];
 
-        int integerValue = minimumValue;
+        long longValue = minimumValue;
         for ( int i = 0; i < numberOfChoices; i++ ) {
-            integerValues[ i ] = NumberFormatUtilities.formatInteger( integerValue, _numberFormat );
-            integerValue += increment;
+            longValues[ i ] = NumberFormatUtilities.formatLong( longValue, _numberFormat );
+            longValue += increment;
         }
 
         // Ensure that most items are visible before scrolling, but also make
@@ -111,7 +109,7 @@ public class IntegerSelector extends NumberSelector {
         setVisibleRowCount( FastMath.min( numberOfChoices, 25 ) );
 
         // Set the non-editable list of supported integer values.
-        setItems( FXCollections.observableArrayList( integerValues ) );
+        setItems( FXCollections.observableArrayList( longValues ) );
 
         // Restrict keyboard input to numerals, sign, and delimiters.
         final String allowedCharacters = ( minimumValue < 0 )
@@ -124,15 +122,15 @@ public class IntegerSelector extends NumberSelector {
         } );
     }
 
-    public final int getIntegerValue() {
+    public final long getLongValue() {
         final String formattedValue = getValue();
-        final int integerValue = NumberFormatUtilities.parseInteger( formattedValue, _numberFormat );
-        return integerValue;
+        final long longValue = NumberFormatUtilities.parseLong( formattedValue, _numberFormat );
+        return longValue;
     }
 
-    public final void setIntegerValue( final int integerValue ) {
-        final String formattedValue = NumberFormatUtilities.formatInteger( integerValue,
-                                                                           _numberFormat );
+    public final void setLongValue( final long longValue ) {
+        final String formattedValue = NumberFormatUtilities.formatLong( longValue,
+                                                                        _numberFormat );
         setValue( formattedValue );
     }
 
