7c5d86fbbabd277f7605aa9a0f5a2eb3958ecfae
Mark Schmieder <markhschmieder@gmail.com>
2025-07-31 11:57:00 -0400

    Update ProjectPropertiesEditor.java
    Made the notes area slightly too wide for 80 characters; it fits 86 characters so we are subtracting 30 pixels.

--- src/main/java/com/mhschmieder/fxguitoolkit/stage/ProjectPropertiesEditor.java
+++ src/main/java/com/mhschmieder/fxguitoolkit/stage/ProjectPropertiesEditor.java
@@ -49,7 +49,7 @@ import javafx.scene.paint.Color;
 public final class ProjectPropertiesEditor extends XStage {
 
     // Default window locations and dimensions.
-    private static final double PROJECT_PROPERTIES_EDITOR_WIDTH_DEFAULT = 620.0d;
+    private static final double PROJECT_PROPERTIES_EDITOR_WIDTH_DEFAULT = 590.0d;
     private static final double  PROJECT_PROPERTIES_EDITOR_HEIGHT_DEFAULT = 400.0d;
 
     // Cache the Project Category for reference during label creation.
@@ -70,7 +70,7 @@ public final class ProjectPropertiesEditor extends XStage {
         // Always call the superclass constructor first!
         // NOTE: The window key prefix is bumped whenever default size changes.
         super( pProjectCategory + " Properties",
-               pProjectCategory.toLowerCase( Locale.US ) + "Properties2",
+               pProjectCategory.toLowerCase( Locale.US ) + "Properties3",
                true,
                true,
                pProductBranding,
